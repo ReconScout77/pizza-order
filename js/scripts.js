857c887758345191b3b71c46d217bb5a75e2c1db
Robert Murray <alexmurray93@hotmail.com>
2017-07-21 10:34:48 -0700

    Add constructor for Pizza objects, add prototypes that modify the topping, size, and price of the pizza.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+  this.price = 5;
+}
+
+Pizza.prototype.addTopping = function() {
+  this.topping = $("#toppings").val();
+  this.price += 1;
+}
+
+Pizza.prototype.chooseSize = function() {
+  this.size = $("#size").val();
+  if (this.size === "Small") {
+    this.price -= 1;
+  } else if (this.size === "Large") {
+    this.price += 3;
+  }
+}
+
+var orderedPizza = new Pizza("none", "Medium");
